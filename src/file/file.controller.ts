a891344d1519a01415f501f67386e567fd5ca3e2
Five <f5.chjr@gmail.com>
2019-10-20 22:23:42 +0700

    feat(file): add `FileService` and `FileController`

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, Res } from '@nestjs/common';
+import { FileService } from './file.service';
 
 @Controller('file')
-export class FileController {}
+export class FileController {
+    constructor(private readonly fileService: FileService) {}
+    @Post('upload')
+    async create(@Req() request, @Res() response) {
+        try {
+            await this.fileService.fileUpload(request, response);
+        } catch (error) {
+            return response
+                .status(500)
+                .json(`Failed to upload image file: ${error.message}`);
+        }
+    }
+}
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req, Res } from '@nestjs/common';
+import * as multer from 'multer';
+import * as AWS from 'aws-sdk';
+import * as multerS3 from 'multer-s3';
 
+const S3: AWS.S3 = new AWS.S3();
+
+// Constants
+const AWS_ACCESS_KEY_ID = '';
+const AWS_SECRET_ACCESS_KEY = '';
+const AWS_S3_BUCKET_NAME = '';
+
+AWS.config.update({
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+});
+
+// TO-DO: REFACTOR
 @Injectable()
-export class FileService {}
+export class FileService {
+    async fileupload(@Req() req, @Res() res) {
+        try {
+            this.upload(req, res, function(error) {
+                if (error) {
+                    // console.log(error);
+                    return res
+                        .status(404)
+                        .json(`Failed to upload file: ${error}`);
+                }
+                return res.status(201).json(req.files[0].location);
+            });
+        } catch (error) {
+            // console.log(error)
+            return res.status(500).json(`Failed to upload file: ${error}`);
+        }
+    }
+
+    upload = multer({
+        storage: multerS3({
+            s3: S3,
+            bucket: AWS_S3_BUCKET_NAME,
+            acl: 'public-read',
+            key: function(request, file, cb) {
+                const fileName = `${Date.now().toString()} - ${
+                    file.originalName
+                }`;
+                cb(null, fileName);
+            },
+        }),
+    }).array('upload', 1);
+}
