3cfa9bdb46a1254d4e09d6ac841dfc4bc861ec6c
Unknown <him10424@gmail.com>
2019-10-26 20:43:46 +0700

    fix: temporary fix for bugs from using dev guard

--- src/auth/auth.decorator.ts
+++ src/auth/auth.decorator.ts
@@ -1,11 +1,6 @@
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { DevEnvironmentGuard } from '../dev-environment.guard';
 
 export const Authenticated = () => {
-    if (process.env.NODE_ENV === 'development') {
-        return UseGuards(DevEnvironmentGuard);
-    } else {
-        return UseGuards(AuthGuard('jwt'));
-    }
+    return UseGuards(AuthGuard('jwt'));
 };
